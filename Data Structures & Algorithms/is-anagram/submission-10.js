85291f5759cfb91b855b37a8e461ac2a07f72327
CHIRAG VERMA <160994025+Chiragverma0786@users.noreply.github.com>
2026-08-26 16:04:09 +0530

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+
+    // Different lengths cannot be anagrams
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const empObj = {};
+
+    for(let i = 0; i<s.length; i++){
+        if(empObj[s[i]]){
+            empObj[s[i]]++;
+        }else{
+            empObj[s[i]] = 1
+        }
+    }
+
+    for(let i = 0; i<t.length; i++){
+        if(empObj[t[i]]){
+            empObj[t[i]]--;
+        }else{
+            empObj[t[i]] = 0
+        }
+    }
+    if(Object.values(empObj).every(value => value === 0)){
+        return true
+    }
+    return false
+    }
+}
